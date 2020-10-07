7cfe2bf43b07d803529821294c67b25d340c856f
SebEil <56046884+SebEil@users.noreply.github.com>
2020-10-07 17:54:50 +0200

    Red tests

---
+++ src/main/resources/db/migration/V001_create_table_workers.sql
@@ -0,0 +1,3 @@
+create table workers (
+    worker_name varchar
+);
--- src/test/java/database/WorkerDaoTest.java
+++ src/test/java/database/WorkerDaoTest.java
@@ -1,11 +1,11 @@
 package database;
 
+import org.flywaydb.core.Flyway;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,9 +16,8 @@ public class WorkerDaoTest {
     void shouldListInsertedWorkers() throws SQLException {
         JdbcDataSource dataSource = new JdbcDataSource();
         dataSource.setUrl("jdbc:h2:mem:testdatabase;DB_CLOSE_DELAY=-1");
-        try (Connection connection = dataSource.getConnection()) {
-            connection.prepareStatement("create table workers (worker_name varchar)").executeUpdate();
-        }
+        Flyway.configure().dataSource(dataSource).load().migrate();
+
 
         WorkerDao workerDao = new WorkerDao(dataSource);
         String worker = exampleWorker();
