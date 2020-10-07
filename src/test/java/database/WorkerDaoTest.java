72f97433c7d46f3ba996805f39897d5cc6253c21
SebEil <56046884+SebEil@users.noreply.github.com>
2020-10-07 18:10:56 +0200

    Create table statement now in migration

--- src/test/java/database/WorkerDaoTest.java
+++ src/test/java/database/WorkerDaoTest.java
@@ -3,11 +3,9 @@ package database;
 import org.flywaydb.core.Flyway;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Random;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WorkerDaoTest {
